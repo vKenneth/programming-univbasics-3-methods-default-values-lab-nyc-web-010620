a279c255084f5a6ae55ff8dead5539a640b160a2
K e n V <kenneth_valdivia@yahoo.com>
2019-12-25 21:26:52 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,8 +1,6 @@
-
-
-puts "What a nutritious meal!"
-puts "A plate of #{protein} with #{veg1} and #{veg2}"
-
 protein = ("Filet Mignon")
 veg1 = ("brocolli")
 veg2 = ("carrots")
+
+puts "What a nutritious meal!"
+puts "A plate of #{protein} with #{veg1} and #{veg2}"
